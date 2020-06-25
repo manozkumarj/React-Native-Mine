349fd094441de9935ae064ad755e02945ba10a74
Manoj Kumar <manojmani517@gmail.com>
2020-06-25 09:10:05 +0530

    Implemented useful modal - with marginertical=50 & bgColor=white && scrollable inside the modal --> Animated-Modals

--- Animated-Modals/components/ModalTester.js
+++ Animated-Modals/components/ModalTester.js
@@ -1,27 +1,109 @@
-import React, { useState } from "react";
-import { Button, Text, View } from "react-native";
+import React, { Component } from "react";
+import {
+  Button,
+  Text,
+  View,
+  StyleSheet,
+  ScrollView,
+  Dimensions,
+} from "react-native";
 import Modal from "react-native-modal";
 
-const ModalTester = () => {
-  const [isModalVisible, setModalVisible] = useState(false);
+const deviceWidth = Dimensions.get("window").width;
+const deviceHeight = Dimensions.get("window").height;
 
-  const toggleModal = () => {
-    setModalVisible(!isModalVisible);
+export default class ModalTester extends Component {
+  state = {
+    isModalVisible: false,
   };
 
-  return (
-    <View style={{ flex: 1 }}>
-      <View style={{ marginVertical: 100 }}>
-        <Button title="Show modal" onPress={toggleModal} />
-      </View>
-      <Modal isVisible={isModalVisible} onBackdropPress={toggleModal}>
-        <View style={{ flex: 1 }}>
-          <Text>Hello!</Text>
-          <Button title="Hide modal" onPress={toggleModal} />
-        </View>
-      </Modal>
-    </View>
-  );
-};
+  toggleModal = () => {
+    this.setState({ isModalVisible: !this.state.isModalVisible });
+  };
 
-export default ModalTester;
+  render() {
+    return (
+      <View style={{ flex: 1 }} style={{ marginVertical: 100 }}>
+        <Button
+          style={{ marginVertical: 100 }}
+          title="Show modal"
+          onPress={this.toggleModal}
+        />
+        <Modal
+          isVisible={this.state.isModalVisible}
+          deviceWidth={deviceWidth}
+          // deviceHeight={300}
+          style={{ backgroundColor: "#fff", marginVertical: 50 }}
+          propagateSwipe={true}
+          Swipeable={true}
+          Scrollable={true}
+          onBackdropPress={this.toggleModal}
+        >
+          <ScrollView style={{ flex: 1 }}>
+            <Text>Hello!</Text>
+            <Button title="Hide modal" onPress={this.toggleModal} />
+
+            <Text style={styles.text}>
+              So, here we have added one Button, and also, we have imported the
+              image file. Right now, we have not used it yet, but we will use it
+              in a minute. Our goal is when the user clicks the button, Modal
+              will pop up otherwise it will not pop up, and we can’t see it. So,
+              now we import one more component and pass the Image and Text as a
+              prop to that component. Also, by default Modal is always open, so
+              we need to handle it our way. That is why we need the state which
+              we can control, and ultimately we control the Modal.
+            </Text>
+            <Text style={styles.text}>
+              So, here we have added one Button, and also, we have imported the
+              image file. Right now, we have not used it yet, but we will use it
+              in a minute. Our goal is when the user clicks the button, Modal
+              will pop up otherwise it will not pop up, and we can’t see it. So,
+              now we import one more component and pass the Image and Text as a
+              prop to that component. Also, by default Modal is always open, so
+              we need to handle it our way. That is why we need the state which
+              we can control, and ultimately we control the Modal.
+            </Text>
+            <Text style={styles.text}>
+              So, here we have added one Button, and also, we have imported the
+              image file. Right now, we have not used it yet, but we will use it
+              in a minute. Our goal is when the user clicks the button, Modal
+              will pop up otherwise it will not pop up, and we can’t see it. So,
+              now we import one more component and pass the Image and Text as a
+              prop to that component. Also, by default Modal is always open, so
+              we need to handle it our way. That is why we need the state which
+              we can control, and ultimately we control the Modal.
+            </Text>
+            <Text style={styles.text}>
+              So, here we have added one Button, and also, we have imported the
+              image file. Right now, we have not used it yet, but we will use it
+              in a minute. Our goal is when the user clicks the button, Modal
+              will pop up otherwise it will not pop up, and we can’t see it. So,
+              now we import one more component and pass the Image and Text as a
+              prop to that component. Also, by default Modal is always open, so
+              we need to handle it our way. That is why we need the state which
+              we can control, and ultimately we control the Modal.
+            </Text>
+            <Text style={styles.text}>
+              So, here we have added one Button, and also, we have imported the
+              image file. Right now, we have not used it yet, but we will use it
+              in a minute. Our goal is when the user clicks the button, Modal
+              will pop up otherwise it will not pop up, and we can’t see it. So,
+              now we import one more component and pass the Image and Text as a
+              prop to that component. Also, by default Modal is always open, so
+              we need to handle it our way. That is why we need the state which
+              we can control, and ultimately we control the Modal.
+            </Text>
+          </ScrollView>
+        </Modal>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 20,
+    paddingHorizontal: 20,
+    // marginLeft: 150,
+  },
+});
