eaa64b3475a59ee3cabe9f4bdf6eda1ec1095abd
Manoj Kumar <manojmani517@gmail.com>
2020-06-25 22:24:57 +0530

    Added WallpaperApp.js file --> Miscellaneous-Functionalities

--- Miscellaneous-Functionalities/App.js
+++ Miscellaneous-Functionalities/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 // import LongPressDetection from "./components/LongPressDetection";
-import HorizontalFlatListScroll from "./components/HorizontalFlatListScroll";
+// import HorizontalFlatListScroll from "./components/HorizontalFlatListScroll";
+import WallpaperApp from "./components/WallpaperApp";
 
 export default function App() {
   return (
@@ -9,7 +10,8 @@ export default function App() {
     //   <Text>Open up App.js to start working on your app!</Text>
     // </View>
     // <LongPressDetection />
-    <HorizontalFlatListScroll />
+    // <HorizontalFlatListScroll />
+    <WallpaperApp />
   );
 }
 
