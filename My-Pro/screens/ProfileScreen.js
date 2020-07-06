3eac1febf7271d3fb4f9b78c4112be461f19d090
Manoj Kumar <manojmani517@gmail.com>
2020-07-06 08:17:39 +0530

    Showing profile user posts under profile menu dynamically, but getting scrollview related warning --> My-Pro

--- My-Pro/screens/ProfileScreen.js
+++ My-Pro/screens/ProfileScreen.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import {
   View,
   Text,
@@ -7,21 +7,33 @@ import {
   TouchableOpacity,
   Image,
   Dimensions,
+  FlatList,
+  RefreshControl,
 } from "react-native";
-import { Ionicons } from "@expo/vector-icons";
+import { Ionicons, Entypo } from "@expo/vector-icons";
 import { useDispatch } from "react-redux";
 import { useNavigation } from "@react-navigation/native";
 import Colors from "./../constants/Colors";
 import Constant from "expo-constants";
 import { useSelector } from "react-redux";
+import { getProfileUserDetailsAndPosts } from "./../redux/actionCreators";
+import { Modalize } from "react-native-modalize";
+
+import DefaultAndCustomBgAndTextColorPost from "./../components/UI/DefaultAndCustomBgAndTextColorPost";
+import CustomBgAndTextAndBorderColorPost from "./../components/UI/CustomBgAndTextAndBorderColorPost";
+import CustomBgAndTextAndCornerPost from "./../components/UI/CustomBgAndTextAndCornerPost";
+import PostReactions from "./../components/UI/PostReactions";
+import PhotosPost from "./../components/UI/PhotosPost";
+
 const defaultAvatar = require("./../assets/images/avatar.png");
 const defaultCoverPic = require("./../assets/images/coverPic.jpeg");
-import { getProfileUserDetailsAndPosts } from "./../redux/actionCreators";
 
 const ProfileScreen = (props) => {
   const { username } = props.route.params;
 
   const [isLoading, setIsLoading] = useState(true);
+  const [isFetching, setIsFetching] = useState(false);
+  const [showablePostId, setShowablePostId] = useState(0);
   let getLoggedInUserDetails = useSelector(
     (state) => state.centralState.loggedInUserDetails
   );
@@ -48,15 +60,54 @@ const ProfileScreen = (props) => {
     isSessionAndProfileUserSame,
     setIsSessionAndProfileUserSame,
   ] = useState(getStatus);
+  const [posts, setPosts] = useState([]);
+  const modalizeRef = useRef(null);
+  const [toggle, setToggle] = useState(true);
   const imagesUrl = "http://192.168.43.22:8088/photo/";
 
   const navigation = useNavigation();
 
   const dispatch = useDispatch();
+  let output = null;
 
   useEffect(() => {
     fetchData();
   }, []);
+
+  const onContentHeightModalOpen = (postId) => {
+    setShowablePostId(postId);
+    modalizeRef.current.open();
+  };
+
+  const handleClose = () => {
+    if (modalizeRef.current) {
+      modalizeRef.current.close();
+    }
+  };
+
+  const getProfileUserPosts = () => {
+    console.log("Make an API call to fetch profile user Posts");
+  };
+
+  const getProfileUserInfo = () => {
+    console.log("Make an API call to fetch profile user Info");
+  };
+
+  const getProfileUserPhotos = () => {
+    console.log("Make an API call to fetch profile user Photos");
+  };
+
+  const getProfileUserFriends = () => {
+    console.log("Make an API call to fetch profile user Friends");
+  };
+
+  const onRefresh = () => {
+    // sendPushNotification();
+    setIsFetching(true);
+    setTimeout(() => {
+      setIsFetching(false);
+    }, 3000);
+  };
 
   const fetchData = async () => {
     try {
@@ -68,6 +119,7 @@ const ProfileScreen = (props) => {
       // console.log(receivedResponse);
       setProfilePageUserDetails(receivedResponse.details.userProfileDetails);
       setLoggedInUserDetails(receivedResponse.details.authUserdetails);
+      setPosts(receivedResponse.details.posts);
       setIsLoading(false);
     } catch (err) {
       setIsLoading(false);
@@ -157,6 +209,152 @@ const ProfileScreen = (props) => {
     );
   }
 
+  let loopId = 1;
+
+  const renderContent = () => (
+    <View style={styles.content}>
+      <Text style={styles.content__subheading}>
+        {"Last step".toUpperCase()}
+      </Text>
+      <Text style={styles.content__heading}>
+        Send the message? - {showablePostId}
+      </Text>
+      <Text style={styles.content__description}>
+        <Text style={styles.text}>
+          So, here we have added one Button, and also, we have imported the
+          image file. Right now, we have not used it yet, but we will use it in
+          a minute. Our goal is when the user clicks the button
+        </Text>
+        <Text style={styles.text}>
+          So, here we have added one Button, and also, we have imported the
+          image file. Right now, we have not used it yet, but we will use it in
+          a minute. Our goal is when the user clicks the button
+        </Text>
+      </Text>
+
+      <TouchableOpacity
+        style={styles.content__description}
+        activeOpacity={0.75}
+        onPress={() => setToggle(!toggle)}
+      >
+        <Text>adjustToContentHeight {JSON.stringify(toggle)}</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={styles.content__button}
+        activeOpacity={0.75}
+        onPress={handleClose}
+      >
+        <Text style={styles.content__buttonText}>{"Send".toUpperCase()}</Text>
+      </TouchableOpacity>
+    </View>
+  );
+
+  if (isFetching) {
+    output = (
+      <View style={styles.loadingContainer}>
+        <Text>Fetching...</Text>
+      </View>
+    );
+  } else if (posts.length === 0) {
+    output = <Text> No posts to show </Text>;
+  } else if (posts.length > 0) {
+    output = (
+      <FlatList
+        data={posts}
+        refreshControl={
+          <RefreshControl
+            refreshing={isFetching}
+            onRefresh={() => onRefresh()}
+            tintColor="red"
+            colors={["red", "green", "blue"]}
+            title="Refreshing..."
+          />
+        }
+        keyExtractor={(item) => item._id + loopId}
+        renderItem={(itemData) => {
+          let displayPage;
+          let userPrimaryDp = itemData.item.postedBy.primaryDp
+            ? imagesUrl + itemData.item.postedBy.primaryDp
+            : defaultAvatar;
+
+          // console.log("userPrimaryDp --> " + userPrimaryDp);
+
+          if (itemData.item.postTypeId === 1 || itemData.item.postTypeId === 3)
+            displayPage = (
+              <DefaultAndCustomBgAndTextColorPost
+                key={loopId++}
+                postData={itemData.item.postProperties}
+                postTypeId={itemData.item.postTypeId}
+              />
+            );
+          else if (itemData.item.postTypeId === 4)
+            displayPage = (
+              <CustomBgAndTextAndBorderColorPost
+                postData={itemData.item.postProperties}
+                postTypeId={itemData.item.postTypeId}
+              />
+            );
+          else if (itemData.item.postTypeId === 2)
+            displayPage = (
+              <PhotosPost
+                postData={itemData.item.postProperties}
+                postTypeId={itemData.item.postTypeId}
+              />
+            );
+          else if (itemData.item.postTypeId === 5)
+            displayPage = (
+              <CustomBgAndTextAndCornerPost
+                postData={itemData.item.postProperties}
+                postTypeId={itemData.item.postTypeId}
+              />
+            );
+
+          let postUserImage;
+          if (itemData.item.postedBy.primaryDp) {
+            postUserImage = (
+              <Image
+                style={styles.postUserDp}
+                source={{
+                  uri: imagesUrl + itemData.item.postedBy.primaryDp,
+                }}
+              />
+            );
+          } else {
+            postUserImage = (
+              <Image style={styles.postUserDp} source={defaultAvatar} />
+            );
+          }
+          return (
+            <View style={styles.singlePostContainer}>
+              <View style={styles.postAndUserDetailsContainer}>
+                <View style={styles.postDpContainer}>{postUserImage}</View>
+                <View style={styles.postUserNameTimeContainer}>
+                  <Text style={{ fontSize: 18, fontWeight: "bold" }}>
+                    {itemData.item.postedBy.fullName}
+                  </Text>
+                  <Text style={{ color: "#000" }}>
+                    5th Jan 2017 - 08:51:25 AM
+                  </Text>
+                </View>
+                <TouchableOpacity style={styles.hrDots} activeOpacity={0.75}>
+                  <Entypo
+                    name="dots-three-horizontal"
+                    size={24}
+                    color="black"
+                    onPress={() => onContentHeightModalOpen(itemData.item._id)}
+                  />
+                </TouchableOpacity>
+              </View>
+              <View>{displayPage}</View>
+              <PostReactions postDetails={itemData.item} />
+            </View>
+          );
+        }}
+      />
+    );
+  }
+
   if (!isLoading && profilePageUserDetails) {
     return (
       <View style={{ flex: 1, marginTop: Constant.statusBarHeight }}>
@@ -177,16 +375,25 @@ const ProfileScreen = (props) => {
             {primaryDpImage}
             {secondaryDpImage}
           </View>
-          <View style={styles.userDetailsContainer}>
-            <Text>
-              This is Profile screen! {profilePageUserDetails.primaryDp}
+          <View style={styles.profileMenu}>
+            <Text style={styles.menuItem} onPress={getProfileUserPosts}>
+              Timeline
             </Text>
-            <Text>{profilePageUserDetails.primaryDp}</Text>
-            <Text>{profilePageUserDetails.secondaryDp}</Text>
-            <Text>{profilePageUserDetails.profileCoverPhoto}</Text>
-            <Text>{username}</Text>
+            <Text style={styles.menuItem} onPress={getProfileUserFriends}>
+              Friends
+            </Text>
+            <Text style={styles.menuItem} onPress={getProfileUserInfo}>
+              About
+            </Text>
+            <Text style={styles.menuItem} onPress={getProfileUserPhotos}>
+              Photos
+            </Text>
           </View>
+          <View style={styles.userDetailsContainer}>{output}</View>
         </ScrollView>
+        <Modalize ref={modalizeRef} adjustToContentHeight={toggle}>
+          {renderContent()}
+        </Modalize>
       </View>
     );
   }
@@ -235,7 +442,113 @@ const styles = StyleSheet.create({
     borderRadius: 100,
   },
   userDetailsContainer: {
+    marginBottom: 30,
+  },
+  profileMenu: {
+    width: "100%",
+    backgroundColor: "#ddd",
+    flexDirection: "row",
+    height: 50,
+    justifyContent: "center",
+    alignItems: "center",
     marginVertical: 30,
+  },
+  menuItem: {
+    flex: 1,
+    alignSelf: "center",
+    textAlign: "center",
+    padding: 10,
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  singlePostContainer: {
+    flex: 1,
+    width: "100%",
+    backgroundColor: "#fff",
+    marginBottom: 20,
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    borderTopColor: "#999",
+    borderBottomColor: "#999",
+  },
+  postAndUserDetailsContainer: {
+    flexDirection: "row",
+    // height: 55,
+    alignItems: "center",
+    padding: 5,
+  },
+  postDpContainer: {
+    height: "100%",
+  },
+  postUserDp: {
+    height: 50,
+    width: 50,
+    borderRadius: 5,
+  },
+  postUserNameTimeContainer: {
+    flex: 2,
+    marginHorizontal: 10,
+  },
+  content__header: {
+    padding: 10,
+    paddingBottom: 0,
+    backgroundColor: Colors.siteColor,
+    borderTopLeftRadius: 15,
+    borderTopRightRadius: 15,
+  },
+
+  content__heading: {
+    marginBottom: 2,
+    fontSize: 18,
+    fontWeight: "600",
+    color: "#fff",
+  },
+
+  content__subheading: {
+    marginBottom: 20,
+    fontSize: 16,
+    color: "#ccc",
+  },
+
+  content__inside: {
+    padding: 15,
+  },
+
+  content__paragraph: {
+    fontSize: 15,
+    fontWeight: "200",
+    lineHeight: 22,
+    color: "#666",
+  },
+
+  content__scrollview: {
+    marginVertical: 20,
+  },
+
+  content__block: {
+    width: 200,
+    height: 80,
+    marginRight: 20,
+    backgroundColor: "#ccc",
+  },
+
+  content__input: {
+    paddingVertical: 15,
+    marginBottom: 10,
+    width: "100%",
+    borderWidth: 1,
+    borderColor: "transparent",
+    borderBottomColor: "#cdcdcd",
+    borderRadius: 6,
+  },
+  hrDots: {
+    paddingHorizontal: 5,
+    // borderWidth: 1,
+    // borderColor: "red",
+  },
+  singleEmoji: {
+    height: 30,
+    width: 30,
   },
 });
 
